2cf15c215d39fde21b4d8ccfae97bada5e9e7691
Andy B <andybarnov@gmail.com>
2019-06-26 16:29:25 +0200

    deliveries create stimulus-style

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :batches, only: :show
+  resources :batch_product, path: :bp, only: [] do
+    resources :deliveries, only: :create
+  end
 end
