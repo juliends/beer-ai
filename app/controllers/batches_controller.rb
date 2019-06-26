3ba93b156ce95f2cf48651ded97f05da8234925b
Daniel Gordon <d_gordon1@icloud.com>
2019-06-26 16:27:07 +0200

    if else -> ternary.

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -2,11 +2,7 @@ class BatchesController < ApplicationController
 
   def show
     # We need to set a batch for first page load
-    if params[:id]
-      @batch = Batch.find(params[:id])
-    else
-      @batch = Batch.first
-    end
+    @batch = params[:id] ? Batch.find(params[:id]) : Batch.first
     @products = @batch.batch_products
   end
 end
