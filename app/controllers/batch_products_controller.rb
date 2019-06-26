b2c5fd85b8245d869302b0b1da9482f5905c78ef
Andy B <andybarnov@gmail.com>
2019-06-26 18:07:47 +0200

    new row continued

--- app/controllers/batch_products_controller.rb
+++ app/controllers/batch_products_controller.rb
@@ -1,6 +1,8 @@
 class BatchProductsController < ApplicationController
   def new
     @batch_product = BatchProduct.new
+    render partial: 'batch_products/new_product_row',
+           locals: { batch_product: @batch_product }
   end
 
   def create
--- app/javascript/controllers/new_row_controller.js
+++ app/javascript/controllers/new_row_controller.js
@@ -3,13 +3,9 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
   static targets = ['rowsWrapper'];
 
-  connect() {
-    console.log(this.rowsWrapperTarget)
-  }
-
   onPostSuccess() {
     const xhr = event.detail[2];
-    this.productRowTarget.innerHTML = xhr.response;
+    this.rowsWrapperTarget.insertAdjacentHTML('beforeend', xhr.response)
   }
 
   onPostError(event) {
