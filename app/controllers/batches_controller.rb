80ce3688438312f1237afaae367cde740db2a821
Daniel Gordon <d_gordon1@icloud.com>
2019-06-26 16:25:40 +0200

    clean up pages controller and set root path w/ fallback.

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,7 +1,12 @@
 class BatchesController < ApplicationController
 
   def show
-    @batch = Batch.find(params[:id])
+    # We need to set a batch for first page load
+    if params[:id]
+      @batch = Batch.find(params[:id])
+    else
+      @batch = Batch.first
+    end
     @products = @batch.batch_products
   end
 end
